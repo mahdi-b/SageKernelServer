6b055250652c316c263e0aa6b0274c7dadde8435
Mahdi  B <b.mahdi@gmail.com>
2023-06-29 13:15:34 -1000

    updated token

--- fastapi/start.sh
+++ fastapi/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /app/SageKernelServer
 git pull
-python fastapi/src/main.py --url http://jupyter_server:8888/?token=123456789
+python fastapi/src/main.py --url http://jupyter_server:8888/?token=123456
